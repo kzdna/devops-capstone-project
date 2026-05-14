84ea7ec1c149f3434f984969a019d9f74653c60e
kzdna <salsadina2005@gmail.com>
2026-05-14 10:31:20 -0400

    fix: resolve flake8 undefined Flask and app redefinition

--- service/routes.py
+++ service/routes.py
@@ -1,10 +1,9 @@
-from flask import jsonify, request, abort
+from flask import jsonify, request, abort, Flask 
 from service import app, status
 from service.models import Account, db
 from flask_cors import CORS 
 from flask_talisman import Talisman
 
-app = Flask(__name__)
 CORS(app) 
 Talisman(app)
 
